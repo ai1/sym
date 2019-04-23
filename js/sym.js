a0d25365069d0db26e4a4e185a3eb2b0ad96130c
ai1 <46782991+ai1@users.noreply.github.com>
2019-04-23 16:49:19 +0600

    Create sym.js

---
+++ js/sym.js
@@ -0,0 +1,12 @@
+var link = document.createElement("link");
+  link.setAttribute("rel","stylesheet");
+  link.setAttribute("href","css.css");
+  var head = document.getElementsByTagName("head")[0];
+  head.appendChild(link);
+
+
+var link = document.createElement("link");
+  link.setAttribute("rel","stylesheet");
+  link.setAttribute("href","csss.css");
+  var head = document.getElementsByTagName("head")[0];
+  head.appendChild(link);
